1f1c31f3a5cfe46e8c7ea0425ef1901ea4b42aca
Thiago Assis <thiagosil@gmail.com>
2020-02-21 09:48:00 -0300

    Creating Deliveryman model

---
+++ src/database/migrations/20200221113712-create-deliveryman.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('deliverymans', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('deliverymans');
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -13,4 +18,7 @@ routes.use(authMiddleware);
 routes.post('/recipients', RecipientController.create);
 routes.put('/recipients/:recipientId', RecipientController.update);
 
+routes.post('/deliveryman', DeliverymanController.create);
+
+routes.post('/files', upload.single('file'), FileController.store);
 module.exports = routes;
